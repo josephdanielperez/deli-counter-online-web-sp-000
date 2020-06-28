bc6fc0646659135071b341526621f7397a133fed
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 06:05:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,11 @@
 def line(katz_deli)
+  phrase = "The line is currently: "
   if katz_deli.length > 0
-    puts "The line is currently: #{katz_deli.join(", ")}"
+    katz_deli.each do |name, index|
+      phrase += "#{index + 1}. #{name} "
+    end
+    puts phrase
   else
-    puts "The line is currently empty."
-  end
-end
-
-def take_a_number(katz_deli)
-  queue = []
-  counter = 1
-  katz_deli.each do |name|
-    queue << "#{counter}. #{name}"
-    puts "Welcome, #{name}. You are #{counter} in line."
-    counter += 1
+    puts "The line is currently empty.""
   end
 end
