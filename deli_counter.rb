4665a6fcbe44905226ec757099a1557311bd3ea2
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 05:56:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   if katz_deli.length > 0
-    puts "The line is currently: #{katz_deli.join()}"
+    puts "The line is currently: #{katz_deli.join(, )}"
   else
     puts "The line is currently empty."
   end
