5152d39b3ccec5408d567be78258bf60c06089e6
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 05:59:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,3 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli)
+  queue = []
+  counter = 1
+  katz_deli.each do |name|
+    queue << "#{counter}. #{name}"
+end
