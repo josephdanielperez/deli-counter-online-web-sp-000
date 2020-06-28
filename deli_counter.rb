c1b33bd4de0428e43f153fa9fbb06797f5161ced
Joseph Perez <hello@josephdanielperez.com>
2020-06-28 06:10:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
